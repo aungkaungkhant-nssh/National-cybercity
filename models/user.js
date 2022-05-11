fffd50bc47b0f532f7edae4144feea8a443d144e
aungkaungkhant-nssh <akkgit0909@gmail.com>
2022-05-11 15:12:14 +0630

    delete cart

--- models/user.js
+++ models/user.js
@@ -29,4 +29,31 @@ module.exports = class User{
             .then((res)=>res)
             .catch((err)=>console.log(err));
     }
+    getCart(){
+        const db = getDB();
+        
+        const productId = [...this.carts.items].map((c)=>c.productId); 
+
+       return db.collection("products")
+            .find({_id:{$in:productId}})
+            .toArray()
+            .then((products)=>{
+                return products.map((p)=>{
+                    return{
+                        ...p,
+                        quantity:this.carts.items.find((c)=> c.productId.toString()=== p._id.toString()).quantity
+                    }
+                })
+            })
+            .catch((err)=>console.log(err))
+
+    }
+    deleteCart(id){
+        const updateCart= this.carts.items.filter((c)=>c.productId.toString()!==id);
+        const db =getDB();
+        return db.collection("users")
+                .updateOne({_id:new mongodb.ObjectId(this._id)},{$set:{carts:{items:updateCart}}})
+                .then((data)=> data)
+                .catch((err)=>console.log(err));
+    }
 }
